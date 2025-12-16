387743b755e06eaa92d9282d3f3b77a3ae0cb205
kosinya <ko.sinya@yandex.ru>
2025-12-16 13:48:53 +0300

    create organizations card

---
+++ src/components/OrganizationCard.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "OrganizationCard"
+}
+</script>
+
+<template>
+  <div class="flex flex-row glass-effect">
+    <img alt="user header" class="w-2" src="https://primefaces.org/cdn/primevue/images/usercard.png" />
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
